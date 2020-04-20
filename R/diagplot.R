a7365fdf90f3ebc3e39a6df2366f8442fb60bbd2
mikemeredith <mmeredith@wcs.org>
2020-04-20 21:15:44 +0800

    Added diagPlot

---
+++ R/diagplot.R
@@ -0,0 +1,13 @@
+# Based closely on wiqid::diagPlot, now for mcmcOutput
+
+# Converts objects to class mcmcOutput and then calls plot.
+
+diagPlot <- function(object, ...) {
+  dots <- list(...)
+  if(is.null(dots$main)) {
+    name <- name <- deparse(substitute(object))
+    plot(mcmcOutput(object), main=paste("Diagnostics for", name), ...)
+  } else {
+    plot(mcmcOutput(object), ...)
+  }
+}
