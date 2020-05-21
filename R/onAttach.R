9b28ab27e7b0e0540acc2a0ebfe68654fbf5f324
Mike Meredith <mmeredith@wcsmsia>
2020-05-21 19:04:27 +0800

    Delete onAttach.R

--- R/onAttach.R
+++
@@ -1,8 +0,0 @@
-
-
-.onAttach <- function(libname, pkgname) {
-  version <- try(utils::packageVersion('mcmcOutput'), silent=TRUE)
-  if(!inherits(version, "try-error"))
-    packageStartupMessage("This is mcmcOutput ", version,
-      ". For overview type ?mcmcOutput.")
-}
